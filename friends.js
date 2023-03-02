7e6b41630b30776cf12c5c825a60f4be44f6567d
Benjamin holden <benjamin.f.holden@outlook.com>
2023-03-01 17:00:45 -0700

    Add cities

---
+++ friends.js
@@ -0,0 +1,7 @@
+var arrays = ["will","amy","george","kathy","jim"]
+console.log(arrays[2])
+console.log(arrays[4])
+console.log(arrays[0])
+var cities = ["denver","chicago","vancouver","seatle","boston"]
+cities.push("orlando")
+console.log(cities)
